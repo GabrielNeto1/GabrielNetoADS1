8a26b41de289800a0db2ef4207e2c0f0e6e1a3b9
GabrielNeto1 <gabrielxv5@gmail.com>
2024-08-22 00:44:32 +0000

    add: código somando balançoTrimestral empresa em java

---
+++ BalancoTrimestral.java
@@ -0,0 +1,13 @@
+class BalancoTrimestral{
+    public static void main(String[] args) {
+        int gastosJaneiro;
+        gastosJaneiro = 15000;
+        int gastosFevereiro;
+        gastosFevereiro = 23000;
+        int gastosMarco;
+        gastosMarco = 17000;
+        int gastosTrimestre = gastosFevereiro + gastosJaneiro + gastosMarco;
+        System.out.println(gastosTrimestre);
+    }
+
+}
